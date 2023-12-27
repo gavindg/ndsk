411b1c3c738bef5400ee015203a201ac73920029
gavindg <52291693+gavindg@users.noreply.github.com>
2023-12-27 15:29:04 -0800

    Test reading clipboard

---
+++ clipboard_test.py
@@ -0,0 +1,31 @@
+import ndsk
+import pyperclip
+from bs4 import BeautifulSoup
+import requests
+import time
+
+
+
+
+def main():
+    last_search = ""
+    # while loop that occasionally checks the clipboard
+    while True:
+        # wait some time
+        try:
+            board = pyperclip.paste()
+            if (last_search != board and ndsk.is_japanese_string(board)):
+                print("readings of", board + ":")
+                jp_search_term, word_scrape_info, kanji_scrape_info = ndsk.parse_input(["ndsk", board, '-k', '-o'])
+                page = requests.get(ndsk.BASE_URL + "%23kanji " + jp_search_term)
+                soup = BeautifulSoup(page.content, "html.parser")
+                out = ndsk.kanji_scrape(soup, kanji_scrape_info)
+                print(out, end="")
+                last_search = board
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+
+
+if __name__ == "__main__":
+    main()
